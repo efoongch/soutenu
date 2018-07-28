f9e0b819d52bfddaeb6f8ab7be05a36bd684a424
Eureka Foong <eureka.foong@gmail.com>
2018-07-28 11:25:44 -0700

    added foundation files

---
+++ generate_button.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class GenerateButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { generated: false };
+  }
+
+  render() {
+    if (this.state.generated == true) {
+      return 'Frappe to the front';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ generated: true }) },
+      'Generate'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#generate_button_container');
+ReactDOM.render(e(GenerateButton), domContainer);
