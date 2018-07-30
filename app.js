311712d02fafc8f411bfeaac1876f44b4e62ac2c
Eureka Foong <eureka.foong@gmail.com>
2018-07-29 18:57:35 -0700

    website overhaul v1

--- app.js
+++ app.js
@@ -54,29 +54,47 @@ function returnStep(dance_moves, counts, counts_left, first, i, all_steps) {
 }
 
 function myFunction() {
+	// Clear current routine list
+	document.getElementById("routine-list").innerHTML = "";
+
 	var total_counts = 0;
-	var max_counts = 16;
+	var max_counts = 16; //Default is 16 for now
 	var i = 0;
 	var all_steps = [];
 	var counts_done = 0;
+	var tendu_moves = ["Tendu en avant", "Tendu à la seconde ", "Tendu derrière", "Passé", "Plié", "Port de bras"];
+	var frappe_moves = ["Frappé en avant", "Frappé à la seconde ", 
+		"Frappé derrière", "Passé", "Petit battement", "Plié", "Port de bras"];
+	var adagio_moves = ["Développé en avant", "Développé à la seconde", "Développé derrière", "Plié", 
+	"Développé derrière into penché", "Attitude en avant", "Attitude derrière"]
+	var dance_moves = tendu_moves.concat(frappe_moves); //By default, use all the moves available
+
+	// Check for routine preferences
+	if (document.getElementById("tendus-option").checked) {
+		dance_moves = tendu_moves;
+	}
+	else if (document.getElementById("frappes-option").checked) {
+		dance_moves = frappe_moves;
+	}
+	else if(document.getElementById("adagio-option").checked) {
+		dance_moves = adagio_moves;
+	}
+
+	// Check for count length preferences
+	if (document.getElementById("3-counts").checked) {
+		max_counts = 3 * 8;
+	}
+	else if (document.getElementById("6-counts").checked) {
+		max_counts = 6 * 8;
+	}
+	else if (document.getElementById("12-counts").checked) {
+		max_counts = 12 * 8;
+	}
 
 	for (i=0; total_counts < max_counts; i++) {
-		var dance_moves = [];
-		var tendu_moves = ["Tendus to the front", "Tendus to the side", "Tendus to the back", "Passe", "Plies"];
-		var frappe_moves = ["Frappes to the front", "Frappes to the side", 
-		"Frappes to the back", "Passe", "Petit battement", "Plies"];
 		var counts = [2, 4, 8, 16];
 		var counts_left = max_counts;
 		var step = {};
-
-		// Check for routine preferences
-		if (document.getElementById("tendus-option").checked) {
-			dance_moves = tendu_moves;
-		}
-		else if (document.getElementById("frappes-option").checked) {
-			dance_moves = frappe_moves;
-		}
-
 
 		// Update counts left to fit the rest of the 8-count 
 		if (total_counts > 0) {
